6dc802663ba2e9878a5fc9b5e80a86d83472e3b5
wtanjo <2403627832@qq.com>
2026-04-27 19:00:06 +0800

    starting to write routers

---
+++ backend/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from routers import tasks, contents, images
+
+app = FastAPI(title="Crawler System MVP")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(tasks.router)
+app.include_router(contents.router)
+
+@app.get("/")
+def welcome():
+    return {"message": "API Running"}
